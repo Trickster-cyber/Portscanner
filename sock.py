e4492a05db87960427fe913fcb401a519fb8a496
Trickster-cyber <49528986+Trickster-cyber@users.noreply.github.com>
2022-04-20 19:46:57 +0530

    Update sock.py

--- sock.py
+++ sock.py
@@ -3,11 +3,11 @@
 import socket,sys,os
 
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-print("Successful")
+print("Socket successfully created")
 
 port = 4444
 host = "127.0.0.1"
 
 s.connect((host,port))
-print("SUccessful connected")
+print("Socket connected")
 
